25505574d5f24b3b629a2935b210be7ea56fe680
zuzubaidillah <zuzubaidillah@gmail.com>
2021-08-22 11:16:37 +0700

    memasang plugin users di server selesai

--- src/server.js
+++ src/server.js
@@ -2,12 +2,20 @@
 require('dotenv').config();
 
 const Hapi = require('@hapi/hapi');
+// notes
 const notes = require('./api/notes');
 const NotesService = require('./services/postgres/NotesService');
 const NotesValidator = require('./validator/notes');
 
+// users
+const UsersService = require('./services/postgres/UsersService');
+const UsersValidator = require('./validator/users');
+const users = require('./api/users');
+
+
 const init = async () => {
   const notesService = new NotesService();
+  const usersService = new UsersService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -18,13 +26,24 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: notes,
-    options: {
-      service: notesService,
-      validator: NotesValidator,
+  // ubah cara registrasi plugin notes dari objek literals menjadi arrays. Tujuannya, agar kita dapat mendaftarkan lebih dari satu plugin sekaligus
+
+  await server.register([
+    {
+      plugin: notes,
+      options: {
+        service: notesService,
+        validator: NotesValidator,
+      },
     },
-  });
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
 
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
